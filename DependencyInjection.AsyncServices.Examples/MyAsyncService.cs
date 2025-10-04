e1c21c324320b5c64a8b34ec2a2e624f4c3d8a0a
Al12rs <26797547+Al12rs@users.noreply.github.com>
2025-10-04 20:55:35 +0200

    Fix missing import

--- DependencyInjection.AsyncServices.Examples/MyAsyncService.cs
+++ DependencyInjection.AsyncServices.Examples/MyAsyncService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using DependencyInjection.AsyncServices.Extensions;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace DependencyInjection.AsyncServices;
